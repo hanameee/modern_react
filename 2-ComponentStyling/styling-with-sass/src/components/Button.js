f0b3ed4ea8aa8c8f0a5a85522902cd55b8958978
hanameee <hanameee@likelion.org>
2019-11-15 23:53:31 +0900

    Initiated styling-with-sass

---
+++ 2-ComponentStyling/styling-with-sass/src/components/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Buttom from "./Button.scss";
+
+function Button({ childeren }) {
+    return <button className="Button">{childeren}</button>;
+}
+
+export default Button;
