836aeb347ace1bd3556437ebebca2032ddc87589
hanameee <hanameee@likelion.org>
2019-11-16 23:16:37 +0900

    Implement dynamic size button using classnames library

--- 2-ComponentStyling/styling-with-sass/src/App.js
+++ 2-ComponentStyling/styling-with-sass/src/App.js
@@ -6,7 +6,9 @@ function App() {
     return (
         <div className="App">
             <div className="buttons">
+                <Button size="small">BUTTON</Button>
                 <Button>BUTTON</Button>
+                <Button size="large">BUTTON</Button>
             </div>
         </div>
     );
--- 2-ComponentStyling/styling-with-sass/src/components/Button.js
+++ 2-ComponentStyling/styling-with-sass/src/components/Button.js
@@ -1,9 +1,11 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
 function Button({ children, size }) {
-    return <button className={["Button", size].join(" ")}>{children}</button>;
+    // return <button className={["Button", size].join(" ")}>{children}</button>;
     // join 말고 className = {`Button ${size}`} 이렇게 할 수도 있다!
+    return <button className={classNames("Button", size)}>{children}</button>;
 }
 
 Button.defaultProps = {
