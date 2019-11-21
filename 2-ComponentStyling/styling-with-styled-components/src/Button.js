8f4de3d3d6ca04a2a69f87caf59145ab58982c63
hanameee <hanameee@likelion.org>
2019-11-21 22:22:20 +0900

    Refactoring - modulize color related styles & use destructuring assignmnet

--- 2-ComponentStyling/styling-with-styled-components/src/Button.js
+++ 2-ComponentStyling/styling-with-styled-components/src/Button.js
@@ -2,6 +2,21 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { darken, lighten } from "polished";
 
+const colorStyles = css`
+    ${({ theme, color }) => {
+        const selected = props.theme.palette[color];
+        return css`
+            background: ${selected};
+            &:hover {
+                background: ${lighten(0.1, selected)};
+            }
+            &:active {
+                background: ${darken(0.1, selected)};
+            }
+        `;
+    }}
+`;
+
 const StyledButton = styled.button`
     /* 공통 스타일 */
     display: inline-flex;
@@ -18,18 +33,7 @@ const StyledButton = styled.button`
     font-size: 1rem;
 
     /* 색상 */
-    ${props => {
-        const selected = props.theme.palette[props.color];
-        return css`
-            background: ${selected};
-            &:hover {
-                background: ${lighten(0.1, selected)};
-            }
-            &:active {
-                background: ${darken(0.1, selected)};
-            }
-        `;
-    }}
+    ${colorStyles}
 
     /* 기타 */
     & + & {
@@ -40,5 +44,9 @@ const StyledButton = styled.button`
 function Button({ children, ...rest }) {
     return <StyledButton {...rest}>{children}</StyledButton>;
 }
+
+Button.defaultProps = {
+    color: "blue"
+};
 
 export default Button;
