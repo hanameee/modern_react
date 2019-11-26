69f74f9bbe6332e1ddb92006f9e70e29682be1ca
hanameee <hanameee@likelion.org>
2019-11-26 22:31:14 +0900

    Add disappearing transition effect to dialog

--- 2-ComponentStyling/styling-with-styled-components/src/Dialog.js
+++ 2-ComponentStyling/styling-with-styled-components/src/Dialog.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes, css } from "styled-components";
 import Button from "./Button";
 
 const fadeIn = keyframes`
@@ -11,12 +11,30 @@ to {
 }
 `;
 
+const fadeOut = keyframes`
+from {
+    opacity: 1
+}
+to {
+    opacity: 0
+}
+`;
+
 const slideUp = keyframes`
 from {
     transform: translateY(200px);
 }
 to {
     transform: translateY(0px);
+}
+`;
+
+const slideDown = keyframes`
+from {
+    transform: translateY(0px);
+}
+to {
+    transform: translateY(200px);
 }
 `;
 
@@ -35,6 +53,12 @@ const DarkBackground = styled.div`
     animation-timing-function: ease-out;
     animation-name: ${fadeIn};
     animation-fill-mode: forwards;
+
+    ${props =>
+        props.disappear &&
+        css`
+            animation-name: ${fadeOut};
+        `}
 `;
 
 const DialogBlock = styled.div`
@@ -54,6 +78,12 @@ const DialogBlock = styled.div`
     animation-timing-function: ease-out;
     animation-name: ${slideUp};
     animation-fill-mode: forwards;
+
+    ${props =>
+        props.disappear &&
+        css`
+            animation-name: ${slideDown};
+        `}
 `;
 
 const ButtonGroup = styled.div`
@@ -77,10 +107,24 @@ function Dialog({
     onCancel,
     visible
 }) {
-    if (!visible) return null;
+    const [animate, setAnimate] = useState(false);
+    const [localVisible, setLocalVisible] = useState(visible);
+    // console.log(`지금 animate는 ${animate}`);
+    // console.log(`지금 visible은 ${visible}`);
+    // console.log(`지금 localvisible은 ${localVisible}`);
+    useEffect(() => {
+        if (localVisible && !visible) {
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 250);
+        }
+        setLocalVisible(visible);
+    }, [localVisible, visible]);
+
+    if (!animate && !localVisible) return null;
+
     return (
-        <DarkBackground>
-            <DialogBlock>
+        <DarkBackground disappear={!visible}>
+            <DialogBlock disappear={!visible}>
                 <h3>{title}</h3>
                 <p>{children}</p>
                 <ButtonGroup>
