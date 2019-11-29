f6092a2698df9d456c3be239396c4b8ec6542af1
hanameee <hanameee@likelion.org>
2019-11-29 16:04:54 +0900

    Add nextId using useRef & error handling with custom useContext hooks

--- 3-TodoList/mashup-todolist/src/TodoContext.js
+++ 3-TodoList/mashup-todolist/src/TodoContext.js
@@ -1,4 +1,4 @@
-import React, { useReducer, createContext, useContext } from "react";
+import React, { useReducer, createContext, useContext, useRef } from "react";
 
 const initialTodos = [
     {
@@ -40,22 +40,43 @@ function todoReducer(state, action) {
 
 const TodoStateContext = React.createContext();
 const TodoDispatchContext = React.createContext();
+const TodoNextIdContext = React.createContext();
 
 export function TodoProvider({ children }) {
     const [state, dispatch] = useReducer(todoReducer, initialTodos);
+    const nextId = useRef(5);
     return (
         <TodoStateContext.Provider value={state}>
             <TodoDispatchContext.Provider value={dispatch}>
-                {childeren}
+                <TodoNextIdContext.Provider value={nextId}>
+                    {childeren}
+                </TodoNextIdContext.Provider>
             </TodoDispatchContext.Provider>
         </TodoStateContext.Provider>
     );
 }
 
+// custom hook for better usability
 export function useTodoState() {
-    return useContext(TodoStateContext);
+    const context = useContext(TodoStateContext);
+    if (!context) {
+        throw new Error("Cannot find TodoProvider");
+    }
+    return context;
 }
 
 export function useTodoDispatch() {
-    return useContext(TodoDispatchContext);
+    const context = useContext(TodoDispatchContext);
+    if (!context) {
+        throw new Error("Cannot find TodoProvider");
+    }
+    return context;
+}
+
+export function useTodoNextId() {
+    const context = useContext(TodoNextIdContext);
+    if (!context) {
+        throw new Error("Cannot find TodoProvider");
+    }
+    return context;
 }
