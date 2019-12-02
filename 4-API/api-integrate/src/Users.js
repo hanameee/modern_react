9b9529429bd43b113566bbcbd7aac5ea7f18c685
hanameee <hanameee@likelion.org>
2019-12-03 00:29:44 +0900

    Add request button

--- 4-API/api-integrate/src/Users.js
+++ 4-API/api-integrate/src/Users.js
@@ -6,24 +6,40 @@ function Users() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const fetchUsers = async () => {
+        try {
+            setError(null);
+            setUsers(null);
+            setLoading(true);
+            const response = await axios.get(
+                "https://jsonplaceholder.typicode.com/users"
+            );
+            setUsers(response.data);
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+    };
+
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                setError(null);
-                setUsers(null);
-                setLoading(true);
-                const response = await axios.get(
-                    "https://jsonplaceholder.typicode.com/users"
-                );
-                setUsers(response.data);
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        };
         fetchUsers();
     }, []);
-    return <div></div>;
+    // 빈 배열을 useEffect의 dependency로 주었으므로 렌더링이 시작되는 시점에 요청이 시작
+    if (loading) return <div>로딩중...</div>;
+    if (error) return <div>에러입니다!</div>;
+    if (!users) return null;
+    return (
+        <>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.username} ({user.name})
+                    </li>
+                ))}
+            </ul>
+            <button onClick={fetchUsers}>다시 불러오기!</button>
+        </>
+    );
 }
 
 export default Users;
