172168dcb3e492be5842c16d29ea90926593b85f
hanameee <hanameee@likelion.org>
2019-12-04 15:59:58 +0900

    Add user specific fetching logic

--- 4-API/api-integrate/src/Users.js
+++ 4-API/api-integrate/src/Users.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import useAsync from "./useAsync";
+import User from "./User";
 
 async function getUsers() {
     const response = await axios.get(
@@ -10,7 +11,9 @@ async function getUsers() {
 }
 
 function Users() {
+    const [userId, setUserId] = useState(null);
     const [state, refetch] = useAsync(getUsers, [], true);
+
     const { loading, data: users, error } = state;
 
     if (loading) return <div>로딩중...</div>;
@@ -21,12 +24,17 @@ function Users() {
         <>
             <ul>
                 {users.map(user => (
-                    <li key={user.id}>
+                    <li
+                        key={user.id}
+                        onClick={() => setUserId(user.id)}
+                        style={{ cursor: "pointer" }}
+                    >
                         {user.username} ({user.name})
                     </li>
                 ))}
             </ul>
             <button onClick={refetch}>다시 불러오기</button>
+            {userId && <User id={userId} />}
         </>
     );
 }
