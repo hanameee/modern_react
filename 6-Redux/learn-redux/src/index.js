fbf9a15ff873c1b1cf084d438103bee3bfc0e400
hanameee <hanameee@likelion.org>
2019-12-16 23:53:11 +0900

    Implement redux-devtools-extension

--- 6-Redux/learn-redux/src/index.js
+++ 6-Redux/learn-redux/src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducers from "./modules/index";
-
-const store = createStore(rootReducers);
+import rootReducer from "./modules/index";
+import { composeWithDevTools } from "redux-devtools-extension";
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
