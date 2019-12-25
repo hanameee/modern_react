0e313ef218cff7703e7e90782bba5abe1b8aadad
hanameee <hanameee@likelion.org>
2019-12-25 23:20:35 +0900

    Render CounterContainer in App

--- 7-ReduxMiddleware/learn-redux-middleware/src/components/Counter.js
+++ 7-ReduxMiddleware/learn-redux-middleware/src/components/Counter.js
@@ -2,7 +2,7 @@ import React from "react";
 function Counter({ number, onIncrease, onDecrease }) {
     return (
         <div>
-            <h1>{number}</h1>
+            <h1>number is :{number}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
         </div>
